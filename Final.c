d9423e554ea7208df866169dbc8e962a4998b79b
Yogeshwar Chaudhari <108257281+YogeshwarChaudhariii@users.noreply.github.com>
2025-12-15 00:23:51 +0530

    Update Final.c

--- Final.c
+++ Final.c
@@ -12,6 +12,7 @@
 void SelectOption();
 void EmiFormulaImplementation();
 void SipFormulaImplementation();
+void FormatAmount(double value, char *output);
 
 ///////////////////////////////////////////////////////////////
 //
@@ -39,12 +40,6 @@ struct Sip
     int InvestmentTenure; // in years 
 };
 
-///////////////////////////////////////////////////////////////
-//
-// Structure : LoginPage
-// Use : Holds information about customer login details
-//
-///////////////////////////////////////////////////////////////
 struct LoginPage
 {
     char UserName[20];
@@ -55,7 +50,7 @@ struct LoginPage
 ///////////////////////////////////////////////////////////////
 //
 // Function : CustomerAccount
-// Use : Customer account details
+// Use : Customer login with username and password
 //
 ///////////////////////////////////////////////////////////////
 void CustomerAccount(struct LoginPage* LoginPage)
@@ -66,7 +61,7 @@ void CustomerAccount(struct LoginPage* LoginPage)
 
     fp = fopen("CustomerLogin.txt", "r");
 
-    if(!fp)
+    if (!fp)
     {
         printf("Error opening file..!\n");
         exit(EXIT_FAILURE);
@@ -80,37 +75,31 @@ void CustomerAccount(struct LoginPage* LoginPage)
     while (1)
     {
         ch = getch();
-
-        // Enter key
-        if(ch == 13)  
-        {
+        if (ch == 13)  // Enter key
             break;
-        }
-        // Backspace
-        else if(ch == 8) 
+        else if (ch == 8) // Backspace
         {
-            if(i > 0)
+            if (i > 0)
             {
                 i--;
                 printf("\b \b");
             }
         }
-        else if(i < 15)
+        else if (i < 15)
         {
             LoginPage->Password[i++] = ch;
             printf("*");
         }
     }
-    
     LoginPage->Password[i] = '\0';
     printf("\n");
 
     // Read file line by line
-    while(fgets(Line, sizeof(Line), fp))
+    while (fgets(Line, sizeof(Line), fp))
     {
-        if(sscanf(Line, "%[^,],%s", FileUser, FilePass) == 2)
+        if (sscanf(Line, "%[^,],%s", FileUser, FilePass) == 2)
         {
-            if(strcmp(FileUser, LoginPage->UserName) == 0 &&
+            if (strcmp(FileUser, LoginPage->UserName) == 0 &&
                 strcmp(FilePass, LoginPage->Password) == 0)
             {
                 Found = 1;
@@ -121,17 +110,59 @@ void CustomerAccount(struct LoginPage* LoginPage)
 
     fclose(fp);
 
-    if(Found)
+    if (Found)
     {
-        printf("\nLogin Successful...\n\n");
+        printf("\nLogin Successful!\n\n");
     }
     else
     {
-        printf("\nInvalid Username or Password...!\n");
+        printf("\nInvalid Username or Password!\n");
         exit(EXIT_FAILURE);
     }
 }
 
+///////////////////////////////////////////////////////////////
+//
+// Function : FormatAmount
+// Use : Format the amount with commas
+//
+///////////////////////////////////////////////////////////////
+void FormatAmount(double value, char *output)
+{
+    char temp[60];
+    int i, j = 0, count = 0, len;
+
+    sprintf(temp, "%.2lf", value);
+    len = strlen(temp);
+
+    for (i = len - 1; i >= 0; i--)
+    {
+        output[j++] = temp[i];
+
+        if (temp[i] == '.')
+        {
+            count = 0;
+            continue;
+        }
+
+        count++;
+
+        if ((count == 3 || (count > 3 && (count - 3) % 2 == 0)) &&
+            i > 0 && temp[i - 1] != '.')
+        {
+            output[j++] = ',';
+        }
+    }
+
+    output[j] = '\0';
+
+    for (i = 0; i < j / 2; i++)
+    {
+        char ch = output[i];
+        output[i] = output[j - i - 1];
+        output[j - i - 1] = ch;
+    }
+}
 
 ///////////////////////////////////////////////////////////////
 //
@@ -145,6 +176,8 @@ void EmiFormulaImplementation()
 
     double CalculateEmi, MonthlyInterestRate, CalculatePower, TotalAmount, TotalInterest;
     int N, iOption;
+
+    char EmiStr[60], PrincipleStr[60], InterestStr[60], TotalStr[60];
 
     printf("Enter the Principle Loan Amount: \n");
     scanf("%lf",&eobj.Principle);
@@ -180,12 +213,18 @@ void EmiFormulaImplementation()
     TotalAmount = CalculateEmi * N;
     TotalInterest = TotalAmount - eobj.Principle;
 
+    // Formatted Amount
+    FormatAmount(CalculateEmi, EmiStr);
+    FormatAmount(eobj.Principle, PrincipleStr);
+    FormatAmount(TotalInterest, InterestStr);
+    FormatAmount(TotalAmount, TotalStr);
+
     // Output
     printf("\n================ EMI DETAILS ================\n");
-    printf("Monthly EMI         : %.2lf rs\n", CalculateEmi);
-    printf("Principal Amount    : %.2lf rs\n", eobj.Principle);
-    printf("Total Interest      : %.2lf rs\n", TotalInterest);
-    printf("Total Amount Payable: %.2lf rs\n", TotalAmount);
+    printf("Monthly EMI         : %s rs\n", EmiStr);
+    printf("Principal Amount    : %s rs\n", PrincipleStr);
+    printf("Total Interest      : %s rs\n", InterestStr);
+    printf("Total Amount Payable: %s rs\n", TotalStr);
     printf("==============================================\n");
 
     printf("\n\n1. Back to Banking Facilities Calculator\n");
@@ -222,6 +261,8 @@ void SipFormulaImplementation()
     double InterestRate, PowerValue, FutureValue, InvestedAmount, Returns;
     int Tenure, iOption;
 
+    char MonthlyStr[60], InvestedStr[60], ReturnsStr[60], FutureStr[60];
+
     printf("Enter the Monthly Investment Amount: \n");
     scanf("%lf",&sobj.MonthlyInvestment);
 
@@ -246,13 +287,19 @@ void SipFormulaImplementation()
     InvestedAmount = sobj.MonthlyInvestment * Tenure;
     Returns = FutureValue - InvestedAmount;
 
+    // Formatted Amount
+    FormatAmount(sobj.MonthlyInvestment, MonthlyStr);
+    FormatAmount(InvestedAmount, InvestedStr);
+    FormatAmount(Returns, ReturnsStr);
+    FormatAmount(FutureValue, FutureStr);
+
 
     printf("\n=========== SIP CALCULATION RESULT ===========\n");
-    printf("Monthly SIP Amount   : %.2lf rs\n", sobj.MonthlyInvestment);
+    printf("Monthly SIP Amount   : %s rs\n", MonthlyStr);
     printf("Total Months         : %d \n", Tenure);
-    printf("Invested Amount      : %.2lf rs\n", InvestedAmount);
-    printf("Total Returns        : %.2lf rs\n", Returns);
-    printf("Final Maturity Value : %.2lf rs\n", FutureValue);
+    printf("Invested Amount      : %s rs\n", InvestedStr);
+    printf("Total Returns        : %s rs\n", ReturnsStr);
+    printf("Final Maturity Value : %s rs\n", FutureStr);
     printf("===============================================\n");
 
     printf("\n\n1. Back to Banking Facilities Calculator\n");
@@ -410,4 +457,3 @@ int main()
 
     return 0;
 }
-
